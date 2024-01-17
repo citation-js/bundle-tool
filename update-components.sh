1a126f009d6a85ee42866464906853e009988cfd
Lars Willighagen <lars.willighagen@gmail.com>
2024-01-17 15:42:35 +0100

    @citation-js@0.7.6; plugin-enw@0.2.0; plugin-isbn@0.4.0; plugin-pubmed@0.3.0; plugin-quickstatements@0.5.2; plugin-software-formats@0.6.1

--- update-components.sh
+++ update-components.sh
@@ -4,4 +4,4 @@ cd node_modules/
 PACKAGES=$(npm outdated --parseable | cut -d: -f4 | grep '^@citation-js' | tr "\n" " ")
 cd ..
 
-npm install $PACKAGES --save-exact
+npm install $PACKAGES --save-exact --legacy-peer-deps
